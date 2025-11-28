ce8e0e6839dc0346b86ceb305fdccf79bb8f180c
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-28 18:15:27 +0200

    Update main.dart

--- khidma/lib/main.dart
+++ khidma/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
 import 'core/app_theme.dart';
 import 'core/app_routes.dart';
 
@@ -17,6 +19,11 @@ class KhidmaApp extends StatelessWidget {
       theme: buildLightTheme(),
       locale: const Locale('ar'),
       supportedLocales: const [Locale('ar'), Locale('en')],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       initialRoute: AppRoutes.onboarding,
       onGenerateRoute: AppRoutes.generateRoute,
     );
