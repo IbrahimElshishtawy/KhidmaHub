01954966ee688abed68782cfd2608f7f5eb9e86d
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-29 22:43:34 +0200

    Update NotificationSettingsPage.dart

---
+++ khidma/lib/features/customer/Notification/NotificationSettingsPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class NotificationSettingsPage extends StatefulWidget {
+  const NotificationSettingsPage({super.key});
+
+  @override
+  State<NotificationSettingsPage> createState() =>
+      _NotificationSettingsPageState();
+}
+
+class _NotificationSettingsPageState extends State<NotificationSettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
