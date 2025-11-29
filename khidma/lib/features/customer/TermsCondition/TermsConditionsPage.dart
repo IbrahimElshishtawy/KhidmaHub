8669b9f1991dacac4e05eb0dad0a59f69682fc67
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-29 22:45:48 +0200

    Update TermsConditionsPage.dart

---
+++ khidma/lib/features/customer/TermsCondition/TermsConditionsPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TermsConditionsPage extends StatefulWidget {
+  const TermsConditionsPage({super.key});
+
+  @override
+  State<TermsConditionsPage> createState() => _TermsConditionsPageState();
+}
+
+class _TermsConditionsPageState extends State<TermsConditionsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
