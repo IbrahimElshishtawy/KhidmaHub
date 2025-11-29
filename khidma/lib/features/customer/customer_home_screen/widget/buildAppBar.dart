80167f1572c58e1dd76bb55b7e242565205c557f
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-29 21:43:32 +0200

    Update buildAppBar.dart

--- khidma/lib/features/customer/customer_home_screen/widget/buildAppBar.dart
+++ khidma/lib/features/customer/customer_home_screen/widget/buildAppBar.dart
@@ -1,15 +1,21 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
+import 'package:khidma/features/customer/NotificationPage.dart';
 
+// Building the app bar, no back button for the Notifications page
 Widget buildAppBar({
+  required BuildContext context,
   required bool isSearching,
   required TextEditingController searchController,
   required Function(bool) onSearchToggle,
   required void Function(String query) onSearchChanged,
+  bool isNotificationPage = false, // Check if the page is for notifications
 }) {
   return AppBar(
     backgroundColor: const Color(0xFF3B5998),
+    automaticallyImplyLeading:
+        !isNotificationPage, // Remove back button for Notifications page
     title: isSearching
         ? TextField(
             controller: searchController,
@@ -31,7 +37,11 @@ Widget buildAppBar({
       IconButton(
         icon: const Icon(Icons.notifications_none),
         onPressed: () {
-          // تنفيذ الكود الخاص بالإشعارات
+          // Navigate to Notifications Page when clicked
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => NotificationPage()),
+          );
         },
       ),
     ],
