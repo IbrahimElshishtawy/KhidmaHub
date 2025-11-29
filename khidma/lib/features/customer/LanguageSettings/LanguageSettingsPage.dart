f2af6f5949945b85da76ee9159d25c77763ad6d6
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-29 22:43:46 +0200

    Create LanguageSettingsPage.dart

---
+++ khidma/lib/features/customer/LanguageSettings/LanguageSettingsPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class LanguageSettingsPage extends StatefulWidget {
+  const LanguageSettingsPage({super.key});
+
+  @override
+  State<LanguageSettingsPage> createState() => _LanguageSettingsPageState();
+}
+
+class _LanguageSettingsPageState extends State<LanguageSettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
