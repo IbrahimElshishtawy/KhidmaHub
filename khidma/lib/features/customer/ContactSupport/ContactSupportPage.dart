d8d777d7e712c24a1554e46c43c4181042f36cd8
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-29 22:43:59 +0200

    Update ContactSupportPage.dart

---
+++ khidma/lib/features/customer/ContactSupport/ContactSupportPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ContactSupportPage extends StatefulWidget {
+  const ContactSupportPage({super.key});
+
+  @override
+  State<ContactSupportPage> createState() => _ContactSupportPageState();
+}
+
+class _ContactSupportPageState extends State<ContactSupportPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
