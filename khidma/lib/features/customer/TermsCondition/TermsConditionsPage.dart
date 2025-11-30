b4159b5330664d7cbd72f7c3e0b941b789ef77b6
Eng Ibrahim Elshishtawy <shishtawyhima@gmail.com>
2025-11-30 20:33:45 +0200

    Update TermsConditionsPage.dart

--- khidma/lib/features/customer/TermsCondition/TermsConditionsPage.dart
+++ khidma/lib/features/customer/TermsCondition/TermsConditionsPage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 
 class TermsConditionsPage extends StatefulWidget {
@@ -10,6 +12,42 @@ class TermsConditionsPage extends StatefulWidget {
 class _TermsConditionsPageState extends State<TermsConditionsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'الشروط والأحكام',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
+            Text(
+              'مرحبًا بك في تطبيقنا. باستخدامك لهذا التطبيق، فإنك توافق على الالتزام بالشروط والأحكام التالية. يرجى قراءتها بعناية قبل استخدام التطبيق.',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            Text(
+              '1. الاستخدام:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'يجب عليك استخدام التطبيق وفقًا للقوانين المحلية والدولية. أي استخدام غير قانوني أو غير مصرح به للتطبيق محظور.',
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 16),
+            Text(
+              '2. التعديلات:',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              'نحتفظ بالحق في تعديل أو تحديث الشروط والأحكام في أي وقت دون إشعار مسبق. يجب عليك مراجعة هذه الصفحة بانتظام للبقاء على اطلاع بأحدث الشروط.',
+              style: TextStyle(fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
